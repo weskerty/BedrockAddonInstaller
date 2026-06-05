18cf805487e2c7584f03688d772d5ace9d3b2841
weskerty <marcoygor0@gmail.com>
2026-06-05 08:06:35 -0300

    GNU/Linux 05/06/2026 08:06

--- scripts/installer.js
+++ scripts/installer.js
@@ -474,7 +474,7 @@ function doInstall(preview, sourceFiles) {
 
   if (fs.existsSync(TEMP_DIR)) fs.rmSync(TEMP_DIR, { recursive: true });
 
-  const doneDir = path.join(DL_DIR, 'Instalados');
+  const doneDir = path.join('Instalados');
   fs.mkdirSync(doneDir, { recursive: true });
   const moved = new Set();
   for (const entry of sourceFiles) {
